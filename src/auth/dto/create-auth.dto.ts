6179cc5ee5a536daf62e01103dd362eba38bbb91
S1SAUR <nikasisauri0@gmail.com>
2024-07-29 22:57:30 +0400

    update dto

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,10 @@
-export class CreateAuthDto {}
+import { IsEmail, IsString } from "class-validator"
+
+export class CreateAuthDto {
+    @IsString()
+    @IsEmail()
+    email: string
+
+    @IsString()
+    password: string
+}
