1a65fc0035ce2361f352b516c9d51cef9a117592
S1SAUR <nikasisauri0@gmail.com>
2024-08-01 16:22:52 +0400

    add authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { Response,Request } from 'express';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  Login(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.LoginUser(createAuthDto);
+  Login(@Body() createAuthDto: CreateAuthDto, @Res({passthrough: true}) response: Response) {
+    
+    return this.authService.LoginUser(createAuthDto,response);
   }
-
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
 
-  constructor( private readonly usersRepository: UsersRepository){}
+  constructor( private readonly usersRepository: UsersRepository,private readonly jwtService: JwtService){}
   create(createUserDto: CreateUserDto) {
     return this.usersRepository.create(createUserDto);
   }
@@ -25,5 +26,23 @@ export class UsersService {
 
   remove(id: number) {
     return this.usersRepository.remove(id);
+  }
+ 
+  
+  async me(request){
+    try{
+      let cookie = request.cookies['jwt']
+
+      let data = await this.jwtService.verifyAsync(cookie)
+
+      if(!data){
+        throw new UnauthorizedException()
+      }
+
+      let user = this.usersRepository.findOne(data.id)
+       return user
+    } catch(err){
+      throw new UnauthorizedException()
+   }
   }
 }
